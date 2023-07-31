adca67c64eaaba5aac71447eb338c7ab064782c0
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-31 18:37:26 +0200

    Create layout.js

---
+++ src/app/(auth)/(routes)/layout.js
@@ -0,0 +1,17 @@
+import { Inter } from "next/font/google";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata = {
+  title: "Accueil || connexion",
+  description:
+    "Estiimea, l'outil ultime pour proposer des estimations immobilieres à vos prospects",
+};
+
+export default function AccueilLayout({ children }) {
+  return (
+    <html lang="fr">
+      <body className={inter.className}>{children}</body>
+    </html>
+  );
+}
