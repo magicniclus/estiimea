da9146afa7acdbd1a5ca7edff0cb32475771ed8b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-07-31 18:37:23 +0200

    Create page.js

---
+++ src/app/(auth)/(routes)/connexion/page.js
@@ -0,0 +1,38 @@
+"use client";
+import React, { useState } from "react";
+import { loginUser } from "@/firebase/auth";
+
+const Page = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const user = await loginUser(email, password);
+      console.log("User logged in:", user);
+    } catch (error) {
+      console.error("Error in handleLogin:", error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <input
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        required
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        required
+      />
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default Page;
