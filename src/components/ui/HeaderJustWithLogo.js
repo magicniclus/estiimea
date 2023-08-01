2a09d11ea6774d83f883753f45934c6499d66fde
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-01 18:27:47 +0200

    Update HeaderJustWithLogo.js

--- src/components/ui/HeaderJustWithLogo.js
+++ src/components/ui/HeaderJustWithLogo.js
@@ -3,12 +3,13 @@ import React from "react";
 const HeaderJustWithLogo = () => {
   return (
     <header className=" mx-auto flex max-w-7xl px-6 lg:px-8">
-      <div className="flex lg:flex-1 py-5 justify-between items-end">
+      <div className="w-full flex lg:flex-1 py-5 justify-between items-end">
         <a href="#" className="-m-1.5 py-1.5 flex items-end">
-          <img className="h-12 w-auto" src="/images/logos/logo_2.png" alt="" />
-          {/* <span className="text-blue-500 font-mont font-normal text-2xl">
-            Estiimea
-          </span> */}
+          <img
+            className="h-12 w-auto"
+            src="/images/logos/logo_2.png"
+            alt="Estiimea"
+          />
         </a>
         <a href="#" className="font-semibold text-slate-700">
           Connexion
