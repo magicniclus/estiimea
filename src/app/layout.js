21d0ea7cb1b6dd917f691cad583ae61a3be7e108
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-01 18:27:44 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,3 +1,4 @@
+import ReduxProvider from "../redux/provider";
 import "./globals.css";
 import { Inter } from "next/font/google";
 
@@ -12,7 +13,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="fr">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <ReduxProvider>{children}</ReduxProvider>
+      </body>
     </html>
   );
 }
