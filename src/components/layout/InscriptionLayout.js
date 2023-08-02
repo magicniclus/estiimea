4527b3795c65768169d75c07f20c97cd7ea9b33d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-02 15:19:34 +0200

    Update InscriptionLayout.js

--- src/components/layout/InscriptionLayout.js
+++ src/components/layout/InscriptionLayout.js
@@ -1,9 +1,17 @@
 import React from "react";
 import HeaderJustWithLogo from "@/components/ui/HeaderJustWithLogo";
+import Head from "next/head";
 
 const InscriptionLayout = (props) => {
+  const title = props.title || "Estimmea - Inscription";
+  const description = props.description || "Estimmea - Inscription";
+
   return (
     <>
+      <Head>
+        <title>{title}</title>
+        <meta name="description" content={description} />
+      </Head>
       <HeaderJustWithLogo />
       <main className="w-full border-t-2 border-slate-100 mx-auto flex max-w-7xl font-mont px-6 lg:px-8 ">
         {props.children}
