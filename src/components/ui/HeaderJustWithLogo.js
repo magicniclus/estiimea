5fce76370e7b2d9f8df936a3d4e11f4fe0f93fff
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-02 15:19:37 +0200

    Update HeaderJustWithLogo.js

--- src/components/ui/HeaderJustWithLogo.js
+++ src/components/ui/HeaderJustWithLogo.js
@@ -1,6 +1,10 @@
+"use client";
 import React from "react";
+import { useRouter, usePathname } from "next/navigation";
 
 const HeaderJustWithLogo = () => {
+  const path = usePathname();
+
   return (
     <header className=" mx-auto flex max-w-7xl px-6 lg:px-8">
       <div className="w-full flex lg:flex-1 py-5 justify-between items-end">
@@ -9,8 +13,11 @@ const HeaderJustWithLogo = () => {
             estimmea
           </h2>
         </a>
-        <a href="#" className="font-semibold text-slate-700">
-          Connexion
+        <a
+          href={path === "/connexion" ? "/inscription" : "/connexion"}
+          className="font-semibold text-slate-700 hover:text-slate-500 trasition duration-150 ease-in-out"
+        >
+          {path === "/connexion" ? "Créer un compte" : "Se connecter"}
         </a>
       </div>
     </header>
