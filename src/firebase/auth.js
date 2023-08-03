9b9d00ae8bbca7c6c1bb73598cc70f05b3377955
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-03 18:57:26 +0200

    Update auth.js

--- src/firebase/auth.js
+++ src/firebase/auth.js
@@ -10,12 +10,11 @@ import {
   onAuthStateChanged,
   signOut,
 } from "firebase/auth";
-import { getDatabase, ref, set, get } from "firebase/database";
 import { app } from "./firebase.config";
 
-//login
+const auth = getAuth(app);
+
 export const loginUser = async (email, password) => {
-  const auth = getAuth(app); // Initialize auth after the app
   try {
     const userCredential = await signInWithEmailAndPassword(
       auth,
@@ -29,7 +28,6 @@ export const loginUser = async (email, password) => {
   }
 };
 
-//User Information
 export const registerUser = async (
   email,
   password,
@@ -41,41 +39,12 @@ export const registerUser = async (
     console.error("Passwords do not match");
     return null;
   }
-
-  const auth = getAuth(app);
   try {
     const userCredential = await createUserWithEmailAndPassword(
       auth,
       email,
       password
     );
-    const db = getDatabase();
-    const userRef = ref(db, `users/${userCredential.user.uid}`); // creates a new reference with the ID of the user's UID
-
-    await set(userRef, {
-      userInformation: {
-        email: email,
-        createdAt: new Date().toISOString(),
-        firstName: firstName || null,
-        lastName: lastName || null,
-        photoProfil:
-          "https://firebasestorage.googleapis.com/v0/b/estiimea.appspot.com/o/pexels-italo-melo-2379004.jpg?alt=media&token=733523fd-13fc-447d-bdf3-70260f0147e4",
-      },
-      settings: {
-        fontColor: "#000000",
-        backgroundColor: "#ffffff",
-      },
-      plan: {
-        name: "Free",
-        price: 0,
-        features: {},
-        subscribeAt: new Date().toISOString(),
-        expireAt: new Date().toISOString() + 30,
-      },
-      estimations: {},
-    });
-
-    console.log("User registered:", userCredential.user);
     return userCredential.user;
   } catch (error) {
     console.error(error);
@@ -83,10 +52,7 @@ export const registerUser = async (
   }
 };
 
-//Email Verification
 export const sendVerificationEmail = async () => {
-  const auth = getAuth(app);
-  console.log(auth.currentUser);
   sendEmailVerification(auth.currentUser)
     .then(() => {
       console.log("Email sent");
@@ -97,12 +63,10 @@ export const sendVerificationEmail = async () => {
 };
 
 export const sendSignInLink = async (email) => {
-  const auth = getAuth(app);
   const actionCodeSettings = {
     url: "http://localhost:3000/connexion",
     handleCodeInApp: true,
   };
-
   sendSignInLinkToEmail(auth, email, actionCodeSettings)
     .then(() => {
       console.log("Email sent");
@@ -113,101 +77,21 @@ export const sendSignInLink = async (email) => {
     });
 };
 
-//Get user
 export const observeAuthState = (userChangeHandler) => {
-  const auth = getAuth(app);
   return onAuthStateChanged(auth, userChangeHandler);
 };
 
-//Google Login
 export const signInWithGoogle = async () => {
-  const auth = getAuth(app);
   const provider = new GoogleAuthProvider();
-
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    const db = getDatabase(app);
-    const userRef = ref(db, `users/${user.uid}`);
-
-    const snapshot = await get(userRef);
-    if (!snapshot.exists()) {
-      set(userRef, {
-        userInformation: {
-          email: user.email,
-          createdAt: new Date().toISOString(),
-          firstName: user.displayName || null,
-          photoProfil:
-            "https://firebasestorage.googleapis.com/v0/b/estiimea.appspot.com/o/pexels-italo-melo-2379004.jpg?alt=media&token=733523fd-13fc-447d-bdf3-70260f0147e4",
-        },
-        settings: {
-          fontColor: "#000000",
-          backgroundColor: "#ffffff",
-        },
-        plan: {
-          name: "Free",
-          price: 0,
-          features: {},
-          subscribeAt: new Date().toISOString(),
-          expireAt: new Date().toISOString() + 30,
-        },
-        estimations: {},
-      }).catch((error) => console.log("Failed to write to database: ", error));
-    }
-
-    return user;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  return signInWithProvider(provider);
 };
 
-//Facebook Login
 export const signInWithFacebook = async () => {
-  const auth = getAuth(app);
   const provider = new FacebookAuthProvider();
-
-  try {
-    const result = await signInWithPopup(auth, provider);
-    const user = result.user;
-    const db = getDatabase(app);
-    const userRef = ref(db, `users/${user.uid}`);
-
-    const snapshot = await get(userRef);
-    if (!snapshot.exists()) {
-      set(userRef, {
-        userInformation: {
-          email: user.email,
-          createdAt: new Date().toISOString(),
-          firstName: user.displayName || null,
-          photoProfil:
-            "https://firebasestorage.googleapis.com/v0/b/estiimea.appspot.com/o/pexels-italo-melo-2379004.jpg?alt=media&token=733523fd-13fc-447d-bdf3-70260f0147e4",
-        },
-        settings: {
-          fontColor: "#000000",
-          backgroundColor: "#ffffff",
-        },
-        plan: {
-          name: "Free",
-          price: 0,
-          features: {},
-          subscribeAt: new Date().toISOString(),
-          expireAt: new Date().toISOString() + 30,
-        },
-        estimations: {},
-      }).catch((error) => console.log("Failed to write to database: ", error));
-    }
-
-    return user;
-  } catch (error) {
-    console.error(error);
-    return null;
-  }
+  return signInWithProvider(provider);
 };
 
-//Logout
 export const logoutUser = async () => {
-  const auth = getAuth(app);
   try {
     await signOut(auth);
     console.log("User logged out");
@@ -216,30 +100,12 @@ export const logoutUser = async () => {
   }
 };
 
-// Get logged-in user data
-export const getLoggedInUserData = async () => {
-  const auth = getAuth(app);
-  const uid = auth.currentUser ? auth.currentUser.uid : null;
-
-  if (uid) {
-    const db = getDatabase(app);
-    const userRef = ref(db, `users/${uid}`);
-
-    try {
-      const snapshot = await get(userRef);
-      if (snapshot.exists()) {
-        // console.log("Logged-in user data:", snapshot.val());
-        return snapshot.val();
-      } else {
-        console.log("No data available for logged-in user id: ", uid);
-        return null;
-      }
-    } catch (error) {
-      console.error("Failed to retrieve logged-in user data: ", error);
-      return null;
-    }
-  } else {
-    console.log("No user is logged in.");
+const signInWithProvider = async (provider) => {
+  try {
+    const result = await signInWithPopup(auth, provider);
+    return result.user;
+  } catch (error) {
+    console.error(error);
     return null;
   }
 };
