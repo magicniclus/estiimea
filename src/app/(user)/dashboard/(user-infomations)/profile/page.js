72ba2c2859da8464db290d1a93510c9a6f21f995
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-04 12:08:40 +0200

    Update page.js

--- src/app/(user)/dashboard/(user-infomations)/profile/page.js
+++ src/app/(user)/dashboard/(user-infomations)/profile/page.js
@@ -1,8 +1,13 @@
 import DashboardLayout from "@/components/layout/DashboardLayout";
+import DisplayUserInformaitons from "@/components/ui/DisplayUserInformaitons";
 import React from "react";
 
 const Page = () => {
-  return <DashboardLayout></DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <DisplayUserInformaitons />
+    </DashboardLayout>
+  );
 };
 
 export default Page;
