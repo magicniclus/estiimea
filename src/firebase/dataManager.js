f78c62e3f3f46e181d40fe7c85a87f63a2fbdf8a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-04 16:08:03 +0200

    Update dataManager.js

--- src/firebase/dataManager.js
+++ src/firebase/dataManager.js
@@ -1,9 +1,14 @@
 // user.js
 import { app } from "./firebase.config";
-import { getDatabase, ref, set, get } from "firebase/database";
+import { getDatabase, ref, set, get, update } from "firebase/database";
 
+// Obtenez une référence à la base de données de Firebase
 const db = getDatabase(app);
 
+// Fonction pour créer un nouvel utilisateur dans la base de données Firebase.
+// Elle prend comme paramètres un ID utilisateur (uid), une adresse email, un prénom et un nom de famille.
+// Elle crée ensuite une nouvelle référence utilisateur dans la base de données avec ces informations,
+// plus quelques autres champs par défaut.
 export const createNewUser = async (uid, email, firstName, lastName) => {
   const userRef = ref(db, `users/${uid}`);
   await set(userRef, {
@@ -12,8 +17,6 @@ export const createNewUser = async (uid, email, firstName, lastName) => {
       createdAt: new Date().toISOString(),
       firstName: firstName || null,
       lastName: lastName || null,
-      photoProfil:
-        "https://firebasestorage.googleapis.com/v0/b/estiimea.appspot.com/o/pexels-italo-melo-2379004.jpg?alt=media&token=733523fd-13fc-447d-bdf3-70260f0147e4",
     },
     settings: {
       fontColor: "#000000",
@@ -30,6 +33,9 @@ export const createNewUser = async (uid, email, firstName, lastName) => {
   });
 };
 
+// Fonction pour récupérer les informations d'un utilisateur connecté.
+// Elle prend un ID utilisateur (uid) comme paramètre et tente d'obtenir les informations de cet utilisateur
+// à partir de la base de données Firebase.
 export const getLoggedInUserData = async (uid) => {
   const userRef = ref(db, `users/${uid}`);
   try {
@@ -43,5 +49,20 @@ export const getLoggedInUserData = async (uid) => {
   } catch (error) {
     console.error("Failed to retrieve logged-in user data: ", error);
     return null;
+  }
+};
+
+// Fonction pour mettre à jour les données d'un utilisateur dans la base de données Firebase.
+// Elle prend un ID utilisateur (uid) et un objet de mises à jour comme paramètres,
+// puis tente de mettre à jour les informations de cet utilisateur avec les mises à jour fournies.
+//updateUserData(uid, { userInformation: { firstName: "newFirstName", lastName: "newLastName" } });
+export const updateUserData = async (uid, updates) => {
+  const userRef = ref(db, `users/${uid}`);
+
+  try {
+    await update(userRef, updates);
+    console.log(`Updated user ${uid} data successfully.`);
+  } catch (error) {
+    console.error("Failed to update user data: ", error);
   }
 };
