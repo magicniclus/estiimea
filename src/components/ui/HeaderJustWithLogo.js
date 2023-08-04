9c70bc83057e56496b146b8e5b74cc85313c3144
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-04 10:13:32 +0200

    Update HeaderJustWithLogo.js

--- src/components/ui/HeaderJustWithLogo.js
+++ src/components/ui/HeaderJustWithLogo.js
@@ -1,25 +1,99 @@
 "use client";
-import React from "react";
-import { useRouter, usePathname } from "next/navigation";
+import { useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 
 const HeaderJustWithLogo = () => {
-  const path = usePathname();
+  const [mobileMenuOpen, setMobileMenuOpen] = useState(false);
 
   return (
-    <header className=" mx-auto flex max-w-7xl px-6 lg:px-8">
-      <div className="w-full flex lg:flex-1 py-5 justify-between items-end">
-        <a href="#" className="-m-1.5 py-1.5 flex items-end">
-          <h2 className="text-4xl text-blue-500 font-normal font-manrope">
-            estimmea
-          </h2>
-        </a>
-        <a
-          href={path === "/connexion" ? "/inscription" : "/connexion"}
-          className="font-semibold text-slate-700 hover:text-slate-500 trasition duration-150 ease-in-out"
-        >
-          {path === "/connexion" ? "Créer un compte" : "Se connecter"}
-        </a>
-      </div>
+    <header className="bg-white">
+      <nav
+        className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8"
+        aria-label="Global"
+      >
+        <div className="flex lg:flex-1">
+          <a href="#" className="-m-1.5 p-1.5">
+            <span className="sr-only">Your Company</span>
+            <img
+              className="h-8 w-auto"
+              src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=500"
+              alt=""
+            />
+          </a>
+        </div>
+        <div className="flex lg:hidden">
+          <button
+            type="button"
+            className="-m-2.5 inline-flex items-center justify-center rounded-md p-2.5 text-gray-700"
+            onClick={() => setMobileMenuOpen(true)}
+          >
+            <span className="sr-only">Open main menu</span>
+            <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+          </button>
+        </div>
+
+        <div className="hidden lg:flex lg:flex-1 lg:justify-end items-center">
+          <a
+            href="/inscription"
+            className="mr-5 rounded-md bg-blue-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-blue-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-bg-blue-500"
+          >
+            Créer un compte
+          </a>
+          <a
+            href="/connexion"
+            className="text-sm font-semibold leading-6 text-slate-700"
+          >
+            Log in <span aria-hidden="true">&rarr;</span>
+          </a>
+        </div>
+      </nav>
+      <Dialog
+        as="div"
+        className="lg:hidden"
+        open={mobileMenuOpen}
+        onClose={setMobileMenuOpen}
+      >
+        <div className="fixed inset-0 z-10" />
+        <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
+          <div className="flex items-center justify-between">
+            <a href="#" className="-m-1.5 p-1.5">
+              <span className="sr-only">Your Company</span>
+              <img
+                className="h-8 w-auto"
+                src="https://tailwindui.com/img/logos/mark.svg?color=blue&shade=500"
+                alt=""
+              />
+            </a>
+            <button
+              type="button"
+              className="-m-2.5 rounded-md p-2.5 text-gray-700"
+              onClick={() => setMobileMenuOpen(false)}
+            >
+              <span className="sr-only">Close menu</span>
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            </button>
+          </div>
+          <div className="mt-6 flow-root">
+            <div className="-my-6 divide-y divide-gray-500/10">
+              <div className="py-6">
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Creer un compte
+                </a>
+                <a
+                  href="#"
+                  className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
+                >
+                  Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </Dialog.Panel>
+      </Dialog>
     </header>
   );
 };
