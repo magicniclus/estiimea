2aaa57fb5f9e4a70042011cd60f5c7e230999273
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-04 11:13:56 +0200

    Update page.js

--- src/app/(user)/dashboard/(fonctionnalités)/page.js
+++ src/app/(user)/dashboard/(fonctionnalités)/page.js
@@ -1,13 +1,43 @@
 "use client";
-import React, { useEffect, useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
 import DashboardLayout from "@/components/layout/DashboardLayout";
+import { getLoggedInUserData } from "@/firebase/dataManager";
+import { observeAuthState } from "@/firebase/auth";
+import { useSelector, useDispatch } from "react-redux";
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
 const Page = () => {
+  const router = useRouter();
+
+  const userState = useSelector((state) => state.user);
+
+  const dispatch = useDispatch();
+
+  //récuperation des information utilisateur si elles ne sont pas déja dans rédux
+  useEffect(() => {
+    dispatch({ type: "SET_USER_LOADING", payload: true });
+    if (!userState) {
+      observeAuthState((user) => {
+        if (user) {
+          getLoggedInUserData(user.uid)
+            .then((userInfo) => {
+              dispatch({ type: "SET_USER_INFORMATION", payload: userInfo });
+            })
+            .catch((error) => {
+              console.error(error);
+            })
+            .finally(() => {
+              dispatch({ type: "SET_USER_LOADING", payload: false });
+            });
+        }
+      });
+    } else dispatch({ type: "SET_USER_LOADING", payload: false });
+  }, [userState]);
+
   return <DashboardLayout></DashboardLayout>;
 };
 
