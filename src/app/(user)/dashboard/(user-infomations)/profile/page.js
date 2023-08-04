c6ef9617b290987d9f23a5686186ed1c584f03e5
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-04 16:07:54 +0200

    Update page.js

--- src/app/(user)/dashboard/(user-infomations)/profile/page.js
+++ src/app/(user)/dashboard/(user-infomations)/profile/page.js
@@ -1,6 +1,7 @@
+"use client";
+import React, { useEffect } from "react";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import DisplayUserInformaitons from "@/components/ui/DisplayUserInformaitons";
-import React from "react";
 
 const Page = () => {
   return (
