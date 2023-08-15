ccbad996e1e4702a588d1b723136000c91b30562
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-15 17:33:57 +0200

    Delete index.js

--- pages/dashboard/settings/index.js
+++
@@ -1,8 +0,0 @@
-import DashboardLayout from "../../../../components/layout/DashboardLayout";
-import React from "react";
-
-const Page = () => {
-  return <DashboardLayout></DashboardLayout>;
-};
-
-export default Page;
