952ce62c82129394ffdb6a084a1a09a73e357e52
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-15 17:33:42 +0200

    Create index.js

---
+++ pages/[slug]/profil/index.js
@@ -0,0 +1,13 @@
+import React, { useEffect } from "react";
+import DisplayUserInformaitons from "../../../components/ui/DisplayUserInformaitons";
+import DashboardLayout from "../../../components/layout/DashboardLayout";
+
+const index = () => {
+  return (
+    <DashboardLayout>
+      <DisplayUserInformaitons />
+    </DashboardLayout>
+  );
+};
+
+export default index;
