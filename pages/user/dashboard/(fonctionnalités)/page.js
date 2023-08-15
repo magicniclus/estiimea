850da367345a0db81803511bdd571e54cd67f54a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-15 16:04:41 +0200

    Create page.js

---
+++ pages/user/dashboard/(fonctionnalités)/page.js
@@ -0,0 +1,16 @@
+"use client";
+import React from "react";
+import { useRouter } from "next/navigation";
+import DashboardLayout from "../../../../components/layout/DashboardLayout";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+const Page = () => {
+  const router = useRouter();
+
+  return <DashboardLayout></DashboardLayout>;
+};
+
+export default Page;
