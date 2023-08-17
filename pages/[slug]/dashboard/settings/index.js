33f381d58711fac05b07def0c580c3e994d6dd00
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-17 11:48:26 +0200

    Create index.js

---
+++ pages/[slug]/dashboard/settings/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import DashboardLayout from "../../../../components/layout/DashboardLayout";
+
+const Page = () => {
+  return <DashboardLayout></DashboardLayout>;
+};
+
+export default Page;
