95982b1cd5018dcbc9d80142452bc0e91e4199b8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 12:45:43 +0200

    Update index.js

--- pages/[slug]/estimation/index.js
+++ pages/[slug]/estimation/index.js
@@ -1,11 +1,18 @@
 import React from "react";
 import EstimationLayout from "../../../components/layout/EstimationLayout";
 import ContainerEstimation from "../../../components/layout/ContainerEstimation";
+import UserInformation from "../../../components/estimation/UserInformation";
+import EtapeEstimationContainer from "../../../components/estimation/EtapeEstimationContainer";
 
 const index = () => {
   return (
     <EstimationLayout>
-      <ContainerEstimation></ContainerEstimation>
+      <ContainerEstimation>
+        <div>
+          <UserInformation />
+          <EtapeEstimationContainer />
+        </div>
+      </ContainerEstimation>
     </EstimationLayout>
   );
 };
