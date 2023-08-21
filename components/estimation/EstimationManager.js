daf73cc94e11cee191d949551e55fe75fa51ae0a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 17:46:01 +0200

    Update EstimationManager.js

--- components/estimation/EstimationManager.js
+++ components/estimation/EstimationManager.js
@@ -1,9 +1,15 @@
 import React from "react";
 import Map from "./Map";
+import ProgressBar from "./ProgressBar";
+import Step from "./Step";
 
 const EstimationManager = () => {
   return (
-    <div className="w-full lg:w-6/12 min-h-[400px] lg:min-h-[600px] flex flex-col justify-end">
+    <div className="w-full lg:w-6/12 min-h-[400px] lg:min-h-[600px] flex flex-col justify-between">
+      <div className="flex flex-col justify-between min-h-[450px]">
+        <ProgressBar />
+        <Step />
+      </div>
       <Map height="80px" />
     </div>
   );
