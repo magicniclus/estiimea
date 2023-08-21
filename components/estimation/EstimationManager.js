42ae94591017dbfc4a359010cb632ed91f7636c8
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 16:45:40 +0200

    Update EstimationManager.js

--- components/estimation/EstimationManager.js
+++ components/estimation/EstimationManager.js
@@ -1,8 +1,11 @@
 import React from "react";
+import Map from "./Map";
 
 const EstimationManager = () => {
   return (
-    <div className="w-full lg:w-6/12 min-h-[400px]  lg:min-h-[600px]"></div>
+    <div className="w-full lg:w-6/12 min-h-[400px] lg:min-h-[600px] flex flex-col justify-end">
+      <Map height="80px" />
+    </div>
   );
 };
 
