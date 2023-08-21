133ba614e1da9584ad09b4adf5d8cb777f858743
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 13:38:48 +0200

    Update EtapeEstimationContainer.js

--- components/estimation/EtapeEstimationContainer.js
+++ components/estimation/EtapeEstimationContainer.js
@@ -1,9 +1,80 @@
-import React from "react";
+import { CheckIcon } from "@heroicons/react/20/solid";
+import React, { useState } from "react";
 
 const EtapeEstimationContainer = () => {
+  //Faire les étapes de l'estimation en fonction de l'avancement de l'utilisateur
+  const [etape, setEtape] = useState([
+    {
+      name: "Adresse",
+      done: true,
+    },
+    {
+      name: "Appartement/Maison",
+      done: false,
+    },
+    {
+      name: "Surface",
+      done: false,
+    },
+    {
+      name: "Pièces",
+      done: false,
+    },
+    {
+      name: "Chambres",
+      done: false,
+    },
+    {
+      name: "Niveaux / Étages",
+      done: false,
+    },
+    {
+      name: "Espaces exterieurs",
+      done: false,
+    },
+    {
+      name: "Standing",
+      done: false,
+    },
+    {
+      name: "Vue",
+      done: false,
+    },
+    {
+      name: "Atouts",
+      done: false,
+    },
+    {
+      name: "Classement energetique",
+      done: false,
+    },
+    {
+      name: "Objectif",
+      done: false,
+    },
+  ]);
   return (
     <div>
-      <h1 className="text-2xl font-light">Carractéristiques de votre bien</h1>
+      <h1 className="text-xl font-light mb-5">
+        Carractéristiques de votre bien
+      </h1>
+      <ul>
+        {etape.map((item, index) => (
+          <li
+            key={index}
+            className={`text-sm ${
+              item.done
+                ? " text-blue-700 font-normal"
+                : " text-gray-400 font-light"
+            } rounded-lg mb-2`}
+          >
+            {item.done ? (
+              <CheckIcon className="inline-block w-4 h-4 mr-2 text-blue-700" />
+            ) : null}
+            {item.name}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
