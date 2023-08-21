ed412519cb1e6bc6ea0da16a7411403f06fd9a17
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 11:35:47 +0200

    Update SearchMapBar.js

--- components/estimation/SearchMapBar.js
+++ components/estimation/SearchMapBar.js
@@ -58,7 +58,7 @@ const SearchMapBar = ({ map }) => {
   };
 
   return (
-    <div>
+    <div className="mb-5">
       <h2 className="text-lg font-normal text-gray-700 mb-3">
         Adresse du bien à estimer
       </h2>
@@ -69,9 +69,9 @@ const SearchMapBar = ({ map }) => {
             value={input}
             onChange={handleInputChange}
             placeholder="Saisir une adresse"
-            className="w-[90%] border border-blue-700 px-4 py-3 rounded-l-md font-light text-gray-900 text-sm"
+            className="max-w-[500px] w-[80%] sm:w-[90%] border border-blue-700 px-4 py-3 rounded-l-md font-light text-gray-900 text-sm"
           />
-          <div className="bg-blue-700 w-[10%] flex items-center justify-center rounded-r-md">
+          <div className="bg-blue-700 w-[20%] sm:w-[10%] flex items-center justify-center rounded-r-md">
             <MapPinIcon className="h-6 w-6 text-white" aria-hidden="true" />
           </div>
           <ul className="absolute top-full bg-white w-full rounded-md shadow-md">
