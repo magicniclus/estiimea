17e1bad9fd4266b1993bec5ae3c95ab758f57303
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 16:45:41 +0200

    Update EtapeEstimationContainer.js

--- components/estimation/EtapeEstimationContainer.js
+++ components/estimation/EtapeEstimationContainer.js
@@ -1,79 +1,85 @@
 import { CheckIcon } from "@heroicons/react/20/solid";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
 
 const EtapeEstimationContainer = () => {
+  const clientInformation = useSelector((state) => state?.clientInformation);
   //Faire les étapes de l'estimation en fonction de l'avancement de l'utilisateur
-  const [etape, setEtape] = useState([
-    {
-      name: "Adresse",
-      done: true,
-    },
-    {
-      name: "Appartement/Maison",
-      done: false,
-    },
-    {
-      name: "Surface",
-      done: false,
-    },
-    {
-      name: "Pièces",
-      done: false,
-    },
-    {
-      name: "Chambres",
-      done: false,
-    },
-    {
-      name: "Niveaux / Étages",
-      done: false,
-    },
-    {
-      name: "Espaces exterieurs",
-      done: false,
-    },
-    {
-      name: "Standing",
-      done: false,
-    },
-    {
-      name: "Vue",
-      done: false,
-    },
-    {
-      name: "Atouts",
-      done: false,
-    },
-    {
-      name: "Classement energetique",
-      done: false,
-    },
-    {
-      name: "Objectif",
-      done: false,
-    },
-  ]);
+  const [etape, setEtape] = useState();
+  useEffect(() => {
+    setEtape([
+      {
+        name: "Adresse",
+        done: clientInformation?.adresse ? true : false,
+      },
+      {
+        name: "Appartement/Maison",
+        done: clientInformation?.typeBien ? true : false,
+      },
+      {
+        name: "Surface",
+        done: clientInformation?.surface ? true : false,
+      },
+      {
+        name: "Pièces",
+        done: clientInformation?.pieces ? true : false,
+      },
+      {
+        name: "Chambres",
+        done: clientInformation?.chambres ? true : false,
+      },
+      {
+        name: "Niveaux / Étages",
+        done: clientInformation?.etages ? true : false,
+      },
+      {
+        name: "Espaces exterieurs",
+        done: clientInformation?.exterieur ? true : false,
+      },
+      {
+        name: "Standing",
+        done: clientInformation?.standing ? true : false,
+      },
+      {
+        name: "Vue",
+        done: clientInformation?.vue ? true : false,
+      },
+      {
+        name: "Atouts",
+        done: clientInformation?.atouts ? true : false,
+      },
+      {
+        name: "Classement energetique",
+        done: clientInformation?.classeEnergie ? true : false,
+      },
+      {
+        name: "Objectif",
+        done: clientInformation?.objectif ? true : false,
+      },
+    ]);
+  }, [clientInformation]);
   return (
-    <div>
+    <div className="lg:block hidden">
       <h1 className="text-xl font-light mb-5">
         Carractéristiques de votre bien
       </h1>
       <ul>
-        {etape.map((item, index) => (
-          <li
-            key={index}
-            className={`text-sm ${
-              item.done
-                ? " text-blue-700 font-normal"
-                : " text-gray-400 font-light"
-            } rounded-lg mb-2`}
-          >
-            {item.done ? (
-              <CheckIcon className="inline-block w-4 h-4 mr-2 text-blue-700" />
-            ) : null}
-            {item.name}
-          </li>
-        ))}
+        {etape &&
+          etape.map((item, index) => (
+            <li
+              key={index}
+              className={`text-sm ${
+                item.done
+                  ? " text-blue-700 font-normal"
+                  : " text-gray-400 font-light"
+              } rounded-lg mb-2`}
+            >
+              {item.done ? (
+                <CheckIcon className="inline-block w-4 h-4 mr-2 text-blue-700" />
+              ) : null}
+              {item.name}
+            </li>
+          ))}
       </ul>
     </div>
   );
