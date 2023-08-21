f9dee04c3f71c6f6de45177c2f831c082ae3b437
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 13:38:34 +0200

    Create EstimationManager.js

---
+++ components/estimation/EstimationManager.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const EstimationManager = () => {
+  return (
+    <div className="w-full lg:w-6/12 min-h-[400px]  lg:min-h-[600px]"></div>
+  );
+};
+
+export default EstimationManager;
