f8ee6966b8438f7601121c50d8cdae7fdf52738a
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 12:16:35 +0200

    Create index.js

---
+++ pages/[slug]/estimation/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import EstimationLayout from "../../../components/layout/EstimationLayout";
+import ContainerEstimation from "../../../components/layout/ContainerEstimation";
+
+const index = () => {
+  return (
+    <EstimationLayout>
+      <ContainerEstimation></ContainerEstimation>
+    </EstimationLayout>
+  );
+};
+
+export default index;
