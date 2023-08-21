523a2857f3be63af726105e86e578210401e0f51
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-21 12:45:36 +0200

    Create EtapeEstimationContainer.js

---
+++ components/estimation/EtapeEstimationContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EtapeEstimationContainer = () => {
+  return (
+    <div>
+      <h1>Carractéristiques de votre bien</h1>
+    </div>
+  );
+};
+
+export default EtapeEstimationContainer;
