db632cf6377ea22d8c18baaf4f851d9e788ae557
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-22 17:07:13 +0200

    Update Step.js

--- components/estimation/Step.js
+++ components/estimation/Step.js
@@ -1,7 +1,29 @@
 import React from "react";
+import Adresse from "./component/Adresse";
+import { useSelector } from "react-redux";
 
 const Step = () => {
-  return <div className="w-full min-h-[360px]"></div>;
+  const primaryColor = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="w-full min-h-[280px] flex flex-col justify-between"
+    >
+      <Adresse />
+      <button
+        type="submit"
+        className={`text-white py-1.5 px-5 rounded-full transition ease-in-out duration-100 w-min`}
+        style={{ backgroundColor: primaryColor }}
+      >
+        Continuer
+      </button>
+    </form>
+  );
 };
 
 export default Step;
