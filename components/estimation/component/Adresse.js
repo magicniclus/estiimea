e5d4f9b9d4c6e4ace62ae6d7b87f44ddd3ad2eab
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-22 17:06:58 +0200

    Create Adresse.js

---
+++ components/estimation/component/Adresse.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import CardWithLogo from "./CardWithLogo";
+import { useSelector, useDispatch } from "react-redux";
+
+const Adresse = () => {
+  const dispatch = useDispatch();
+  const primaryColor = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
+  const [selectedType, setSelectedType] = useState(null);
+
+  const handleTypeSelection = (type) => {
+    setSelectedType(type);
+  };
+
+  useEffect(() => {
+    if (selectedType !== null) {
+      dispatch({
+        type: "SET_CLIENT_INFORMATION",
+        payload: { type: selectedType },
+      });
+    } else
+      dispatch({
+        type: "SET_CLIENT_INFORMATION",
+        payload: { type: null },
+      });
+  }, [selectedType]);
+
+  return (
+    <>
+      <h2 className="text-2xl font-light" style={{ color: primaryColor }}>
+        Type de bien :
+      </h2>
+      <div className="flex">
+        <CardWithLogo
+          margin="35px"
+          onSelect={handleTypeSelection} // Ajout du prop onSelect ici
+          typeSelected={selectedType}
+        />
+        <CardWithLogo
+          type="immeuble"
+          onSelect={handleTypeSelection} // Ajout du prop onSelect ici
+          typeSelected={selectedType}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Adresse;
