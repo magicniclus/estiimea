27329f8e97e4cefda9373c1ba658554f1473d198
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-24 14:17:39 +0200

    Update Step.js

--- components/estimation/Step.js
+++ components/estimation/Step.js
@@ -1,6 +1,12 @@
 import React from "react";
-import Adresse from "./component/Adresse";
 import { useSelector } from "react-redux";
+
+import AppartementMaison from "./component/AppartementMaison";
+import Surface from "./component/Surface";
+import NbrPieces from "./component/NbrPieces";
+import NbrChambres from "./component/NbrChambres";
+import Annee from "./component/Annee";
+import NiveauxEtage from "./component/NiveauxEtage";
 
 const Step = () => {
   const primaryColor = useSelector(
@@ -9,12 +15,37 @@ const Step = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+  const component = "niveauEtage";
+  const handleComponent = () => {
+    switch (component) {
+      case "AppartementMaison":
+        return <AppartementMaison />;
+
+      case "surface":
+        return <Surface />;
+
+      case "NbrPieces":
+        return <NbrPieces />;
+
+      case "NbrChambres":
+        return <NbrChambres />;
+
+      case "annee":
+        return <Annee />;
+
+      case "niveauEtage":
+        return <NiveauxEtage />;
+
+      default:
+        return <AppartementMaison />;
+    }
+  };
   return (
     <form
       onSubmit={handleSubmit}
       className="w-full min-h-[280px] h-full flex flex-col justify-between"
     >
-      <Adresse />
+      {handleComponent()}
       <button
         type="submit"
         className={`text-white py-1.5 px-5 rounded-full transition ease-in-out duration-100 w-min lg:mb-0 my-10`}
