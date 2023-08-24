aa442f4818fc0e514987a7cd79699364a274d9ed
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-24 14:17:11 +0200

    Create Annee.js

---
+++ components/estimation/component/Annee.js
@@ -0,0 +1,44 @@
+import React, { useState, useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import SelectWithIcon from "./SelectWithIcon";
+
+const Annee = () => {
+  const dispatch = useDispatch();
+  const primaryColor = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
+  const [value, setValue] = useState(0);
+
+  const options = [
+    "Avant 1949",
+    "Entre 1949 et 1997",
+    "Entre 1997 et 2005",
+    "Entre 2005 et 2012",
+    "Entre 2012 et 2018",
+    "Après 2018",
+    "Neuf",
+  ];
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_CLIENT_INFORMATION",
+      payload: { annee: value },
+    });
+  }, [value]);
+
+  return (
+    <>
+      <h2
+        className="text-2xl font-light lg:my-0 my-5"
+        style={{ color: primaryColor }}
+      >
+        Surfaces de votre bien :
+      </h2>
+      <div className="flex">
+        <SelectWithIcon options={options} onChange={setValue} />
+      </div>
+    </>
+  );
+};
+
+export default Annee;
