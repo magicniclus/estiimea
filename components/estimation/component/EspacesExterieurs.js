1bdb4a36ce2743e7594792add54edd0b672d71b1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-24 16:27:03 +0200

    Update EspacesExterieurs.js

--- components/estimation/component/EspacesExterieurs.js
+++ components/estimation/component/EspacesExterieurs.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
-import SelectWithIcon from "./SelectWithIcon";
 import CardWithSize from "./CardWithSize";
 
 const EspacesExterieurs = () => {
