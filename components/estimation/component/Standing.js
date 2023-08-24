be7ea32f05c3c830a18e2c8e3158f082e29555e7
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-24 16:27:05 +0200

    Create Standing.js

---
+++ components/estimation/component/Standing.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CardWithoutSize from "./CardWithoutSize";
+
+const Standing = () => {
+  const [selectedCard, setSelectedCard] = useState("");
+  const dispatch = useDispatch();
+  const primaryColor = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
+
+  useEffect(() => {
+    dispatch({
+      type: "SET_CLIENT_INFORMATION",
+      payload: { standing: selectedCard },
+    });
+  }, [selectedCard]);
+
+  return (
+    <>
+      <h2
+        className="text-2xl font-light lg:my-0 my-5 w-full max-w-[350px]"
+        style={{ color: primaryColor }}
+      >
+        Quel est le standing de votre bien:
+      </h2>
+      <div className="flex w-full justify-between flex-wrap max-w-[400px]">
+        <CardWithoutSize
+          title="Moyen"
+          selectedCard={selectedCard}
+          setSelectedCard={setSelectedCard}
+        />
+        <CardWithoutSize
+          title="Standard"
+          selectedCard={selectedCard}
+          setSelectedCard={setSelectedCard}
+        />
+        <CardWithoutSize
+          title="Exceptionnel"
+          selectedCard={selectedCard}
+          setSelectedCard={setSelectedCard}
+        />
+      </div>
+    </>
+  );
+};
+
+export default Standing;
