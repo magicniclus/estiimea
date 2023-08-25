a2ebc321f74a0ae99d0987fa165e853b98cac183
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-25 12:59:33 +0200

    Update EtapeEstimationContainer.js

--- components/estimation/EtapeEstimationContainer.js
+++ components/estimation/EtapeEstimationContainer.js
@@ -13,26 +13,32 @@ const EtapeEstimationContainer = () => {
       {
         name: "Adresse",
         done: clientInformation?.adresse ? true : false,
+        select: stepInProgress === 1 ? true : false,
       },
       {
         name: "Appartement/Maison",
         done: clientInformation?.type ? true : false,
+        select: stepInProgress === 2 ? true : false,
       },
       {
         name: "Surface",
         done: clientInformation?.surface ? true : false,
+        select: stepInProgress === 3 ? true : false,
       },
       {
         name: "Pièces",
         done: clientInformation?.pieces ? true : false,
+        select: stepInProgress === 4 ? true : false,
       },
       {
         name: "Chambres",
         done: clientInformation?.chambres ? true : false,
+        select: stepInProgress === 5 ? true : false,
       },
       {
         name: "Année",
         done: clientInformation?.annee ? true : false,
+        select: stepInProgress === 6 ? true : false,
       },
       clientInformation?.type === "Appartement"
         ? {
@@ -41,18 +47,22 @@ const EtapeEstimationContainer = () => {
               clientInformation?.etages && clientInformation?.niveaux
                 ? true
                 : false,
+            select: stepInProgress === 7 ? true : false,
           }
         : {
             name: "Niveaux",
             done: clientInformation?.etages ? true : false,
+            select: stepInProgress === 7 ? true : false,
           },
       {
         name: "Espaces exterieurs",
         done: stepInProgress >= 9 ? true : false,
+        select: stepInProgress === 8 ? true : false,
       },
       {
         name: "Standing",
         done: clientInformation?.standing ? true : false,
+        select: stepInProgress === 9 ? true : false,
       },
       {
         name: "Vue / oriantation",
@@ -60,6 +70,7 @@ const EtapeEstimationContainer = () => {
           clientInformation?.vue && clientInformation?.oriantation.length !== 0
             ? true
             : false,
+        select: stepInProgress === 10 ? true : false,
       },
       {
         name: "Travaux",
@@ -68,19 +79,23 @@ const EtapeEstimationContainer = () => {
           clientInformation?.travaux !== undefined
             ? true
             : false,
+        select: stepInProgress === 11 ? true : false,
       },
       {
         name: "Atouts",
         done: stepInProgress >= 12 ? true : false,
+        select: stepInProgress === 12 ? true : false,
       },
       {
         name: "Classement energetique",
         done: clientInformation?.dpe && clientInformation?.ges ? true : false,
+        select: stepInProgress === 13 ? true : false,
       },
       {
         name: "Objectif",
         done:
           clientInformation?.contrat && clientInformation?.vente ? true : false,
+        select: stepInProgress === 14 ? true : false,
       },
     ]);
   }, [clientInformation]);
@@ -96,8 +111,10 @@ const EtapeEstimationContainer = () => {
               key={index}
               className={`text-sm ${
                 item.done
-                  ? " text-blue-700 font-normal"
-                  : " text-gray-400 font-light"
+                  ? " text-blue-700"
+                  : stepInProgress === index + 1
+                  ? "text-gray-400 font-bold"
+                  : "text-gray-400 font-normal"
               } rounded-lg mb-2`}
             >
               {item.done ? (
