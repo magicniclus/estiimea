cc35c97b5d26bb5668da7e9292d87f531dea754b
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-25 12:52:26 +0200

    Update InputNumber.js

--- components/estimation/component/InputNumber.js
+++ components/estimation/component/InputNumber.js
@@ -4,10 +4,13 @@ const InputNumber = (props) => {
   const value = props.value;
   const setValue = props.onChange;
   const type = props.type;
+  const placeholder = props.placeholder;
 
   const handleInputChange = (event) => {
     const inputValue = parseInt(event.target.value, 10);
-    setValue(inputValue);
+    if (Number.isInteger(Number(inputValue))) {
+      setValue(parseInt(inputValue, 10));
+    }
   };
 
   return (
@@ -15,6 +18,7 @@ const InputNumber = (props) => {
       <input
         type="number"
         value={value}
+        step="1"
         onChange={handleInputChange}
         className="ax-w-[500px] w-[80%] sm:w-[90%] border border-blue-700 px-4 py-3 rounded-l-md font-light text-gray-900 text-sm"
         placeholder="Entrez la surface..."
