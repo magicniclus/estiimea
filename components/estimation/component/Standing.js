b55006dea42ad775b6dafc8de7657ad57ecd4aff
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-26 20:03:54 +0200

    Update Standing.js

--- components/estimation/component/Standing.js
+++ components/estimation/component/Standing.js
@@ -5,7 +5,8 @@ import CardWithoutSize from "./CardWithoutSize";
 const Standing = () => {
   const [selectedCard, setSelectedCard] = useState("");
   const dispatch = useDispatch();
-  const primaryColor = useSelector(
+  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
+  const secondaryColor = useSelector(
     (state) => state?.user?.settings?.fontColor2
   );
 
@@ -20,7 +21,7 @@ const Standing = () => {
     <>
       <h2
         className="text-2xl font-light lg:my-0 my-5 w-full max-w-[350px]"
-        style={{ color: primaryColor }}
+        style={{ color: secondaryColor }}
       >
         Quel est le standing de votre bien:
       </h2>
