e9cd6253d0e7bc7a028ec756211ae70dec3b9622
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-26 20:03:29 +0200

    Update CardWithSize.js

--- components/estimation/component/CardWithSize.js
+++ components/estimation/component/CardWithSize.js
@@ -3,7 +3,8 @@ import { useSelector } from "react-redux";
 
 const CardWithSize = (props) => {
   const { title, selected, setSelected } = props;
-  const primaryColor = useSelector(
+  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
+  const secondaryColor = useSelector(
     (state) => state?.user?.settings?.fontColor2
   );
   const handleClick = () => {
@@ -17,16 +18,19 @@ const CardWithSize = (props) => {
   return (
     <div
       onClick={handleClick}
-      className={`mr-5 mt-2  relative w-24 py-5 bg-white border-blue-700 border rounded-md flex justify-center items-center cursor-pointer transition-all duration-100 hover:shadow-lg ${
+      className={`mr-5 mt-2  relative w-24 py-5 bg-white border rounded-md flex justify-center items-center cursor-pointer transition-all duration-100 hover:shadow-lg ${
         selected ? "shadow-md" : null
       }`}
+      style={{ borderColor: secondaryColor }}
     >
-      <p className="text-blue-700 font-light text-sm">{title}</p>
+      <p className=" font-light text-sm" style={{ color: secondaryColor }}>
+        {title}
+      </p>
       <div className="w-4 h-4 bg-gray-200 rounded-full absolute top-1 right-1">
         <div
           className="w-3 h-3 rounded-full absolute top-0.5 right-0.5"
           style={{
-            backgroundColor: primaryColor,
+            backgroundColor: secondaryColor,
             display: selected ? "" : "none",
           }}
         ></div>
