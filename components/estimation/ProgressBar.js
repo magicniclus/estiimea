71868b12a508628f4a05dfb713084ed59d02ca13
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-26 20:04:12 +0200

    Update ProgressBar.js

--- components/estimation/ProgressBar.js
+++ components/estimation/ProgressBar.js
@@ -6,6 +6,10 @@ import { useRouter } from "next/router";
 const ProgressBar = () => {
   const step = useSelector((state) => state.stepInProgress);
   const initialStep = useSelector((state) => state.simulateurStep);
+  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
+  const secondaryColor = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
 
   const dispatch = useDispatch();
 
@@ -34,14 +38,19 @@ const ProgressBar = () => {
     <div className="lg:mt-3 mt-10">
       <div className="relative w-full h-2 bg-gray-200 rounded-md z-0">
         <div
-          className={`ml-0.5 absolute bg-blue-500 rounded-md h-[80%] top-1/2 transform -translate-y-1/2 transition-all duration-500 ease-out`}
-          style={{ width: `${(step * 100) / initialStep}%` }}
+          className={`ml-0.5 absolute rounded-md h-[80%] top-1/2 transform -translate-y-1/2 transition-all duration-500 ease-out`}
+          style={{
+            width: `${(step * 100) / initialStep}%`,
+            backgroundColor: secondaryColor,
+          }}
         ></div>
       </div>
       {step !== 0 ? (
         <button onClick={handleClick} className="flex mt-2">
-          <ChevronLeftIcon className=" text-gray-700 w-4" />
-          <p className="text-xs font-light font-gray-700">Retour</p>
+          <ChevronLeftIcon className=" w-4" style={{ color: primaryColor }} />
+          <p className="text-xs font-light" style={{ color: primaryColor }}>
+            Retour
+          </p>
         </button>
       ) : null}
     </div>
