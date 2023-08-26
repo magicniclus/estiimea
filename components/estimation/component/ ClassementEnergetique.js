a6303e468b9b213339e997b1e1b95d183a8c72fa
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-26 20:03:12 +0200

    Update  ClassementEnergetique.js

--- components/estimation/component/ ClassementEnergetique.js
+++ components/estimation/component/ ClassementEnergetique.js
@@ -5,9 +5,11 @@ import SelectWithIcon from "./SelectWithIcon";
 const ClassementEnergetique = () => {
   const options = ["A", "B", "C", "D", "E", "F", "G"];
   const dispatch = useDispatch();
-  const primaryColor = useSelector(
+  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
+  const secondaryColor = useSelector(
     (state) => state?.user?.settings?.fontColor2
   );
+
   const [valueDPE, setValueDPE] = useState(0);
   const [valueGES, setValueGES] = useState(0);
   useEffect(() => {
@@ -20,17 +22,27 @@ const ClassementEnergetique = () => {
     <div>
       <h2
         className="text-2xl font-light lg:my-0 my-5"
-        style={{ color: primaryColor }}
+        style={{ color: secondaryColor }}
       >
         Classement énergetique de votre bien :
       </h2>
       <div className="mt-5">
         <div className="flex flex-col">
-          <h3 className="font-light text-sm text-gray-700 mb-3">DPE </h3>
+          <h3
+            className="font-light text-smmb-3"
+            style={{ color: primaryColor }}
+          >
+            DPE{" "}
+          </h3>
           <SelectWithIcon options={options} onChange={setValueDPE} />
         </div>
         <div className="flex flex-col mt-5">
-          <h3 className="font-light text-sm text-gray-700 mb-3">GSE </h3>
+          <h3
+            className="font-light text-sm mb-3"
+            style={{ color: primaryColor }}
+          >
+            GSE{" "}
+          </h3>
           <SelectWithIcon options={options} onChange={setValueGES} />
         </div>
       </div>
