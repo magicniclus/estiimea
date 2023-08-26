bb10f486271aed80cfaacc61d2e9c3c9abbd9a8e
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-26 20:03:27 +0200

    Update CardWithoutSize.js

--- components/estimation/component/CardWithoutSize.js
+++ components/estimation/component/CardWithoutSize.js
@@ -5,7 +5,8 @@ const CardWithoutSize = (props) => {
   const { title, selectedCard, setSelectedCard, handleCardChange } = props;
   const typeOfCard = props.typeOfCard || null;
   const standing = useSelector((state) => state?.clientInformation?.standing);
-  const primaryColor = useSelector(
+  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
+  const secondaryColor = useSelector(
     (state) => state?.user?.settings?.fontColor2
   );
   const isSelected = typeOfCard ? selectedCard : title === selectedCard;
@@ -21,16 +22,19 @@ const CardWithoutSize = (props) => {
   return (
     <div
       onClick={handleClick}
-      className={`mr-5 xs:mr-0 relative w-24 py-5 bg-white border-blue-700 border rounded-md flex justify-center items-center cursor-pointer transition-all duration-100 hover:shadow-lg mt-2 ${
+      className={`mr-5 xs:mr-0 relative w-24 py-5 bg-white border rounded-md flex justify-center items-center cursor-pointer transition-all duration-100 hover:shadow-lg mt-2 ${
         isSelected ? "shadow-md" : ""
       }`}
+      style={{ borderColor: secondaryColor }}
     >
-      <p className="text-blue-700 font-light text-sm">{title}</p>
+      <p className="font-light text-sm" style={{ color: secondaryColor }}>
+        {title}
+      </p>
       <div className="w-4 h-4 bg-gray-200 rounded-full absolute top-1 right-1">
         <div
           className="w-3 h-3 rounded-full absolute top-0.5 right-0.5"
           style={{
-            backgroundColor: primaryColor,
+            backgroundColor: secondaryColor,
             display: isSelected ? "" : "none",
           }}
         ></div>
