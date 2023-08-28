bcfdf70943b1ec059fbe6e5092b63b8a4c768bf1
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-28 10:57:17 +0200

    Create AnalyseLoader.js

---
+++ components/estimation/component/AnalyseLoader.js
@@ -0,0 +1,103 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+import LittleLoader from "../../loader/LittleLoader";
+import { CheckIcon } from "@heroicons/react/20/solid";
+
+const AnalyseLoader = () => {
+  const [showCheckIcon, setShowCheckIcon] = useState(false);
+  const [showCheckIconTwo, setShowCheckIconTwo] = useState(false);
+  const [showCheckIconThree, setShowCheckIconThree] = useState(false);
+  const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
+  const secondaryColor = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowCheckIcon(true);
+    }, 3000);
+
+    return () => clearTimeout(timer); // Clear the timer if the component is unmounted
+  }, []);
+
+  useEffect(() => {
+    if (showCheckIcon) {
+      const timer = setTimeout(() => {
+        setShowCheckIconTwo(true);
+      }, 3000);
+      return () => clearTimeout(timer); // Clear the timer if the component is unmounted
+    }
+  }, [showCheckIcon]);
+
+  useEffect(() => {
+    if (showCheckIconTwo) {
+      const timer = setTimeout(() => {
+        setShowCheckIconThree(true);
+      }, 3000);
+      return () => clearTimeout(timer); // Clear the timer if the component is unmounted
+    }
+  }, [showCheckIconTwo]);
+
+  return (
+    <div className="w-full lg:w-6/12 min-h-[400px] lg:min-h-[600px] flex flex-col justify-center items-center">
+      <h2
+        className="text-2xl lg:my-0 my-5 text-center"
+        style={{ color: secondaryColor }}
+      >
+        Analyse en cours...
+      </h2>
+      <div className="mt-5">
+        <div className="flex items-center">
+          {showCheckIcon ? (
+            <CheckIcon className="h-8 w-8 text-green-500" />
+          ) : (
+            <LittleLoader />
+          )}
+          <p
+            className="ml-3"
+            style={{
+              color: primaryColor,
+              opacity: showCheckIcon ? "1" : "0.8",
+            }}
+          >
+            Analyse de vos données
+          </p>
+        </div>
+        <div className="flex items-center mt-3">
+          {showCheckIconTwo ? (
+            <CheckIcon className="h-8 w-8 text-green-500" />
+          ) : (
+            <LittleLoader />
+          )}
+          <p
+            className="ml-3"
+            style={{
+              color: primaryColor,
+              opacity: showCheckIconTwo ? "1" : "0.8",
+            }}
+          >
+            Analyse du marché
+          </p>
+        </div>
+        <div className="flex items-center mt-3">
+          {showCheckIconThree ? (
+            <CheckIcon className="h-8 w-8 text-green-500" />
+          ) : (
+            <LittleLoader />
+          )}
+          <p
+            className="ml-3"
+            style={{
+              color: primaryColor,
+              opacity: showCheckIconThree ? "1" : "0.8",
+            }}
+          >
+            Analyse de vos coordonnées
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default AnalyseLoader;
