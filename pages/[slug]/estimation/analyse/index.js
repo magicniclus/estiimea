f6507c3dad4bc1fa5c234d164849c2327408566d
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-28 12:20:24 +0200

    Update index.js

--- pages/[slug]/estimation/analyse/index.js
+++ pages/[slug]/estimation/analyse/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import { useRouter } from "next/router";
 
@@ -9,8 +9,10 @@ import EstimationLayout from "../../../../components/layout/EstimationLayout";
 import UserInformation from "../../../../components/estimation/UserInformation";
 import AnalysePresentation from "../../../../components/estimation/component/AnalysePresentation";
 import AnalyseLoader from "../../../../components/estimation/component/AnalyseLoader";
+import Loader from "../../../../components/loader/Loader";
 
 const index = () => {
+  const [isLoading, setIsLoading] = useState(false);
   const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
   const secondaryColor = useSelector(
     (state) => state?.user?.settings?.fontColor2
@@ -19,12 +21,23 @@ const index = () => {
   const pathSegments = router.asPath.split("/");
   const currentSlug = pathSegments[1];
 
+  useEffect(() => {
+    if (primaryColor) {
+      setIsLoading(true);
+    }
+  }, [primaryColor]);
+
   const handleRoute = () => {
     router.push(`/${currentSlug}/estimation/analyse`);
   };
   return (
     <EstimationLayout>
       <ContainerEstimation>
+        {!isLoading && (
+          <div className="fixed top-0 left-0 w-full h-full bg-white flex items-center justify-center z-10">
+            <Loader />
+          </div>
+        )}
         <div className="lg:min-h-[600px] flex flex-col justify-between  w-full lg:w-4/12">
           <UserInformation />
           <AnalysePresentation />
