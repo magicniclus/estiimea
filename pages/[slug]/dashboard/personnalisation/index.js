1b34f901834d5da07a7ff619feb1b2b75dd96e83
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-29 18:12:28 +0200

    Update index.js

--- pages/[slug]/dashboard/personnalisation/index.js
+++ pages/[slug]/dashboard/personnalisation/index.js
@@ -1,8 +1,31 @@
-import React from "react";
-import DashboardLayout from "../../../../components/layout/DashboardLayout";
+import React, { useState } from "react";
+import PersonnalisationEstimationLayout from "../../../../components/layout/PersonnalisationEstimationLayout";
+import SideBar from "../../../../components/SideBar";
+import { useSelector } from "react-redux";
+import Estimation from "../../../../components/estimation/Estimation";
+import PeronalisationLayout from "../../../../components/layout/PeronalisationLayout";
 
 const index = () => {
-  return <DashboardLayout></DashboardLayout>;
+  const stateFontColor = useSelector(
+    (state) => state?.user?.settings?.fontColor
+  );
+  const stateFontColor2 = useSelector(
+    (state) => state?.user?.settings?.fontColor2
+  );
+  const stateUserIsLoading = useSelector((state) => state?.mapIsLoading);
+  const stateClientAdresse = useSelector(
+    (state) => state?.clientInformation?.adresse
+  );
+  const [openSideBar, setOpenSideBar] = useState(true);
+  return (
+    <PeronalisationLayout>
+      <div className="flex justify-center mt-5">
+        <PersonnalisationEstimationLayout>
+          <Estimation />
+        </PersonnalisationEstimationLayout>
+      </div>
+    </PeronalisationLayout>
+  );
 };
 
 export default index;
