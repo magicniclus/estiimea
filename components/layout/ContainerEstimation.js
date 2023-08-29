099fd2e472aaec509fb0209a7d3c1a50e82bb203
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-29 18:12:18 +0200

    Update ContainerEstimation.js

--- components/layout/ContainerEstimation.js
+++ components/layout/ContainerEstimation.js
@@ -9,7 +9,7 @@ const ContainerEstimation = (props) => {
     (state) => state?.user?.settings?.fontColor2
   );
   return (
-    <div className="lg:w-10/12 sm:w-11/12 w-full max-w-[1250px] min-h-[665px] px-5 py-10 lg:px-20 lg:py-16 bg-white shadow-lg flex items-center justify-between flex-col lg:flex-row relative z-20">
+    <div className="lg:w-full sm:w-11/12 w-full max-w-[1250px] min-h-[665px] px-5 py-10 lg:px-20 lg:py-16 bg-white shadow-lg flex items-center justify-between flex-col lg:flex-row relative z-20">
       <div className="absolute -top-1 -right-1 z-20 w-32 h-32 overflow-hidden lg:block hidden">
         <a
           href="/"
