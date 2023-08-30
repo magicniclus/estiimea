522dd638b20c230acba55cb9ef7897a8f830d18c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-08-30 15:31:03 +0200

    Update AnalysePresentation.js

--- components/estimation/component/AnalysePresentation.js
+++ components/estimation/component/AnalysePresentation.js
@@ -1,48 +1,37 @@
-import React, { useRef, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
-import { gsap } from "gsap";
 
 const AnalysePresentation = () => {
   const textValue = ["votre demande", "vos données", "le marché"];
+  const [currentText, setCurrentText] = useState(textValue[0]);
   const primaryColor = useSelector((state) => state?.user?.settings?.fontColor);
   const secondaryColor = useSelector(
     (state) => state?.user?.settings?.fontColor2
   );
 
-  const spanContainerRef = useRef(null);
-  const spanRef = useRef(null);
-  let currentIndex = 0;
+  const [isVisible, setIsVisible] = useState(true);
 
   useEffect(() => {
+    let currentIndex = 0;
+
     const changeText = () => {
-      gsap.to(spanRef.current, {
-        y: "-100%",
-        duration: 0.5,
-        ease: "power2.out",
-        onComplete: () => {
-          spanRef.current.innerHTML =
-            textValue[currentIndex % textValue.length];
-          gsap.set(spanRef.current, { y: "100%" }); // Reset to the start position
-          gsap.to(spanRef.current, {
-            y: "0%",
-            duration: 0.5,
-            ease: "power2.in",
-          });
-          currentIndex++;
-        },
-      });
+      setIsVisible(false);
+      setTimeout(() => {
+        currentIndex = (currentIndex + 1) % textValue.length;
+        setCurrentText(textValue[currentIndex]);
+        setIsVisible(true);
+      }, 500);
     };
 
     const intervalId = setInterval(changeText, 2000);
-
     return () => clearInterval(intervalId);
-  }, [textValue]);
+  }, []);
 
   return (
     <div className="lg:block hidden">
       <div>
         <h1
-          className="text-xl lg:text-3xl lg:w-FULL lg:mt-0 mt-10 "
+          className="text-xl lg:text-3xl lg:w-FULL lg:mt-0 mt-10"
           style={{ color: primaryColor }}
         >
           Patientez quelques secondes, analysons{" "}
@@ -53,18 +42,16 @@ const AnalysePresentation = () => {
               display: "flex",
               alignItems: "center",
             }}
-            ref={spanContainerRef}
           >
             <span
-              ref={spanRef}
-              className="text-xl lg:text-4xl"
-              style={{
-                color: secondaryColor,
-                display: "block",
-                transform: "translateY(0%)",
-              }}
+              className={`text-xl lg:text-4xl transition-all ease-in-out duration-500 ${
+                isVisible
+                  ? "opacity-100 transform translateY(0)"
+                  : "opacity-0 -translate-y-1/2"
+              }`}
+              style={{ color: secondaryColor }}
             >
-              {textValue[0]}
+              {currentText}
             </span>
           </div>
         </h1>
