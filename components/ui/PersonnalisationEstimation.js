3a260d26908d5077eb248912254b92c84da8f357
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-01 17:03:53 +0200

    Update PersonnalisationEstimation.js

--- components/ui/PersonnalisationEstimation.js
+++ components/ui/PersonnalisationEstimation.js
@@ -1,17 +1,100 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SelectedButton from "./SelectedButton";
-import TextArea from "./TextArea";
-import HTMLArea from "../HTMLArea";
-import { useSelector } from "react-redux";
+// import HTMLArea from "../HTMLArea";
+import { useSelector, useDispatch } from "react-redux";
+import { updateUserDataSettings } from "../../firebase/dataManager";
+import dynamic from "next/dynamic";
 
+const HTMLArea = dynamic(() => import("../HTMLArea"), {
+  ssr: false,
+});
 const PersonnalisationEstimation = () => {
+  const dispatch = useDispatch();
+
+  const [hasChanges, setHasChanges] = useState(false);
+
+  const originalDescriptionOne = useSelector(
+    (state) => state?.user?.settings?.description
+  );
+
+  const originalDescriptionTwo = useSelector(
+    (state) => state?.user?.settings?.description2
+  );
+
+  const originalEntreprise =
+    useSelector((state) => state?.user?.settings?.entreprise) || "";
+
+  const originalSelected =
+    useSelector((state) => state?.user?.settings?.contract) || "Mandataire";
+
   const [descriptionOne, setDescriptionOne] = useState(
     useSelector((state) => state?.user?.settings?.description)
-  );
+  ); //user/settings/descritpion
 
   const [descriptionTwo, setDescriptionTwo] = useState(
     useSelector((state) => state?.user?.settings?.description2)
-  );
+  ); //user/settings/descritpion2
+
+  const [entreprise, setEntreprise] = useState(
+    useSelector((state) => state?.user?.settings?.entreprise) || ""
+  ); //user/settings/entreprise
+
+  const [selected, setSelected] = useState(
+    useSelector((state) => state?.user?.settings?.contract) || "Mandataire"
+  ); //user/settings/contract
+
+  const uid = useSelector((state) => state?.user?.uid);
+
+  useEffect(() => {
+    if (
+      descriptionOne !== originalDescriptionOne ||
+      descriptionTwo !== originalDescriptionTwo ||
+      entreprise !== originalEntreprise ||
+      selected !== originalSelected
+    ) {
+      setHasChanges(true);
+    } else {
+      setHasChanges(false);
+    }
+  }, [
+    descriptionOne,
+    descriptionTwo,
+    entreprise,
+    selected,
+    originalDescriptionOne,
+    originalDescriptionTwo,
+    originalEntreprise,
+    originalSelected,
+  ]);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const updates = {
+      "settings/description": descriptionOne,
+      "settings/description2": descriptionTwo,
+      "settings/entreprise": entreprise,
+      "settings/contract": selected,
+    };
+
+    await updateUserDataSettings(uid, updates);
+
+    // Dispatchez l'action après la mise à jour de Firebase
+    const userInformation = {
+      description: descriptionOne,
+      description2: descriptionTwo,
+      entreprise: entreprise,
+      contract: selected,
+      // Si vous avez une image de profil à inclure
+      // photoProfil: img  // Assurez-vous d'avoir défini "img" précédemment
+    };
+
+    dispatch({
+      type: "UPDATE_SETTINGS",
+      payload: userInformation,
+    });
+  };
+
   return (
     <div className="grid grid-cols-1 gap-x-8 gap-y-8 pt-10 md:grid-cols-3">
       <div className="px-4 sm:px-0">
@@ -24,7 +107,10 @@ const PersonnalisationEstimation = () => {
         </p>
       </div>
 
-      <form className="bg-white shadow-sm ring-1 ring-gray-700/5 rounded-md md:col-span-2">
+      <form
+        onSubmit={handleSubmit}
+        className="bg-white shadow-sm ring-1 ring-gray-700/5 rounded-md md:col-span-2"
+      >
         <div className=" px-4 py-6 sm:p-8">
           <div className="sm:col-span-4 ">
             <label
@@ -36,11 +122,10 @@ const PersonnalisationEstimation = () => {
             <div className="mt-2">
               <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 sm:max-w-md outline-none">
                 <input
-                  type="email"
-                  name="email"
-                  id="email"
                   className="block flex-1 border-0 bg-transparent py-1.5 text-gray-700 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6 outline-none pl-2"
                   placeholder="IAD, Safti, Capifrance, ..."
+                  value={entreprise}
+                  onChange={(e) => setEntreprise(e.target.value)}
                 />
               </div>
             </div>
@@ -54,22 +139,18 @@ const PersonnalisationEstimation = () => {
             </label>
             <div className="mt-2">
               <div className="w-full rounded-md outline-none">
-                <SelectedButton />
+                <SelectedButton value={selected} setValue={setSelected} />
               </div>
             </div>
           </div>
           <HTMLArea
-            onChange={(content) => {
-              setDescriptionOne(content); // ou toute autre logique que vous souhaitez implémenter
-            }}
+            setDescription={setDescriptionOne}
             value={descriptionOne}
             max={150}
           />
 
           <HTMLArea
-            onChange={(content) => {
-              setDescriptionTwo(content); // ou toute autre logique que vous souhaitez implémenter
-            }}
+            setDescription={setDescriptionTwo}
             value={descriptionTwo}
             title="Modifier la seconde descritption"
             max={70}
@@ -85,7 +166,10 @@ const PersonnalisationEstimation = () => {
           </button>
           <button
             type="submit"
-            className="rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm bg-blue-700 "
+            disabled={!hasChanges}
+            className={`rounded-md px-3 py-2 text-sm font-semibold text-white shadow-sm ${
+              hasChanges ? "bg-blue-700" : "bg-gray-400 cursor-not-allowed"
+            }`}
           >
             Enregistrer
           </button>
