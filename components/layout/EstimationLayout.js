bcbd70818f38923e78c38cbde720a12559497b6c
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-09-01 11:10:24 +0200

    Update EstimationLayout.js

--- components/layout/EstimationLayout.js
+++ components/layout/EstimationLayout.js
@@ -1,36 +1,41 @@
 import React from "react";
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getLoggedInUserData } from "../../firebase/dataManager";
-import { observeAuthState } from "../../firebase/auth";
+import { getUserDataBySlug } from "../../firebase/dataManager";
+import { useRouter } from "next/router";
 
 const EstimationLayout = (props) => {
   const dispatch = useDispatch();
   const userState = useSelector((state) => state.user);
 
+  // Utilisez useRouter pour obtenir le slug
+  const router = useRouter();
+  const slug = router.query.slug;
+
   //récuperation des information utilisateur si elles ne sont pas déja dans rédux
   useEffect(() => {
     dispatch({ type: "SET_USER_LOADING", payload: true });
+    console.log(slug);
     if (!userState) {
-      observeAuthState((user) => {
-        if (user) {
-          getLoggedInUserData(user.uid)
-            .then((userInfo) => {
-              dispatch({
-                type: "SET_USER_INFORMATION",
-                payload: { ...userInfo, uid: user.uid },
-              });
-            })
-            .catch((error) => {
-              console.error(error);
-            })
-            .finally(() => {
-              dispatch({ type: "SET_USER_LOADING", payload: false });
+      getUserDataBySlug(slug) // Assurez-vous que 'slug' est accessible
+        .then((userInfo) => {
+          if (userInfo) {
+            dispatch({
+              type: "SET_USER_INFORMATION",
+              payload: userInfo,
             });
-        }
-      });
+          } else {
+            // Vous pouvez gérer le cas où l'utilisateur n'existe pas ici
+          }
+        })
+        .catch((error) => {
+          console.error(error);
+        })
+        .finally(() => {
+          dispatch({ type: "SET_USER_LOADING", payload: false });
+        });
     } else dispatch({ type: "SET_USER_LOADING", payload: false });
-  }, [userState]);
+  }, [slug, userState]); // Assurez-vous que 'slug' est une dépendance
 
   return (
     <main className="w-full min-h-screen flex justify-center items-center bg-grayPrimary">
