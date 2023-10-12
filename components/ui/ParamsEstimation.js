f691862a20a8c1910a6075b7d9d3c2f59c0ba039
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-12 17:56:17 +0200

    Update ParamsEstimation.js

--- components/ui/ParamsEstimation.js
+++ components/ui/ParamsEstimation.js
@@ -1,12 +1,14 @@
 import React from "react";
 import ParamsProfilInformations from "./ParamsProfilInformations";
 import PersonnalisationEstimation from "./PersonnalisationEstimation";
+import ParamsTraker from "./ParamsTraker";
 
 const ParamsEstimation = (props) => {
   return (
     <div className="space-y-10 divide-y divide-gray-900/10 bg-slate-50 px-5 py-5 rounded-md mt-10">
       <ParamsProfilInformations />
       <PersonnalisationEstimation />
+      <ParamsTraker />
     </div>
   );
 };
