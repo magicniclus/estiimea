2033d4f8ee776cb9ee844d9625d9753a343f4a09
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-16 14:28:41 +0200

    Update index.js

--- pages/[slug]/dashboard/mes-estimations/index.js
+++ pages/[slug]/dashboard/mes-estimations/index.js
@@ -1,8 +1,13 @@
 import React from "react";
 import DashboardLayout from "../../../../components/layout/DashboardLayout";
+import Tables from "../../../../components/ui/Tables";
 
 const index = () => {
-  return <DashboardLayout></DashboardLayout>;
+  return (
+    <DashboardLayout>
+      <Tables />
+    </DashboardLayout>
+  );
 };
 
 export default index;
