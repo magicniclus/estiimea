12acc15719d9b8c49d0aefe280c6dace9e8078df
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-16 16:41:43 +0200

    Update index.js

--- pages/[slug]/dashboard/mes-estimations/index.js
+++ pages/[slug]/dashboard/mes-estimations/index.js
@@ -1,11 +1,21 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import DashboardLayout from "../../../../components/layout/DashboardLayout";
 import Tables from "../../../../components/ui/Tables";
+import { useSelector } from "react-redux";
 
 const index = () => {
+  const userState = useSelector((state) => state.user);
+  const [estimations, setEstimations] = useState([]);
+
+  useEffect(() => {
+    if (userState?.estimations) {
+      setEstimations(userState.estimations);
+    }
+  }, [userState]);
+
   return (
     <DashboardLayout>
-      <Tables />
+      <Tables data={estimations} />
     </DashboardLayout>
   );
 };
