69b3af5f8e0590f43acebcbccd7d5a1d3d099aef
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-17 17:07:15 +0200

    Update Tables.js

--- components/ui/Tables.js
+++ components/ui/Tables.js
@@ -1,19 +1,22 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { capitalizeFirstLetter } from "../../lib/utils";
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/20/solid";
 
-const Tables = (props) => {
-  const [getEstimations, setEstimations] = useState([]);
-
-  useEffect(() => {
-    if (props.data) {
-      // Si les données sont un objet, convertissez-les en tableau, sinon utilisez-les telles quelles
-      const dataToSet = Array.isArray(props.data) ? props.data : [props.data];
-      setEstimations(dataToSet);
+const Tables = ({ data, onSort, sortedColumn }) => {
+  const renderIcon = (column) => {
+    if (sortedColumn.name !== column) {
+      return <ChevronDownIcon className="w-4 h-4 ml-1 opacity-10" />;
     }
-  }, [props.data]);
+
+    return sortedColumn.direction === "asc" ? (
+      <ChevronUpIcon className="w-4 h-4 ml-1" />
+    ) : (
+      <ChevronDownIcon className="w-4 h-4 ml-1" />
+    );
+  };
 
   return (
-    <div className="px-4 sm:px-6 lg:px-8 mt-5">
+    <div className="mt-5">
       <div className="mt-8 flow-root">
         <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
           <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
@@ -21,30 +24,19 @@ const Tables = (props) => {
               <table className="min-w-full divide-y divide-gray-300">
                 <thead className="bg-gray-50">
                   <tr>
-                    <th
-                      scope="col"
-                      className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6"
-                    >
-                      Nom, prénom
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Adresse
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Type de bien
-                    </th>
-                    <th
-                      scope="col"
-                      className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
-                    >
-                      Date
-                    </th>
+                    {["firstName", "adresse", "type", "date"].map((column) => (
+                      <th
+                        key={column}
+                        scope="col"
+                        className="py-3.5 px-3 text-left text-sm font-semibold text-gray-900 cursor-pointer"
+                        onClick={() => onSort(column)}
+                      >
+                        <div className="flex items-center">
+                          {capitalizeFirstLetter(column)}
+                          {renderIcon(column)}
+                        </div>
+                      </th>
+                    ))}
                     <th
                       scope="col"
                       className="relative py-3.5 pl-3 pr-4 sm:pr-6"
@@ -54,7 +46,7 @@ const Tables = (props) => {
                   </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-200 bg-white">
-                  {getEstimations.map((estimation, idx) => (
+                  {data.map((estimation, idx) => (
                     <tr key={estimation.email + idx}>
                       <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                         {capitalizeFirstLetter(estimation.firstName)}{" "}
