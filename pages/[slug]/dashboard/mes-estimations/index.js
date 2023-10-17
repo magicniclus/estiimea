0bf1e39adfa220129a59ccd6025bf55cd16e28bf
Nicolas <72094122+magicniclus@users.noreply.github.com>
2023-10-17 17:07:17 +0200

    Update index.js

--- pages/[slug]/dashboard/mes-estimations/index.js
+++ pages/[slug]/dashboard/mes-estimations/index.js
@@ -8,41 +8,81 @@ const Index = () => {
   const userState = useSelector((state) => state.user);
   const [estimations, setEstimations] = useState([]);
   const [initialEstimations, setInitialEstimations] = useState([]);
+  const [filterValue, setFilterValue] = useState("");
+  const [sortConfig, setSortConfig] = useState({
+    column: null,
+    direction: "asc",
+  });
 
-  // Mise à jour des estimations lorsqu'elles sont disponibles dans userState
   useEffect(() => {
     if (userState?.estimations && !initialEstimations.length) {
-      setEstimations([...userState.estimations]);
       setInitialEstimations([...userState.estimations]);
     }
-  }, [userState]); // Ajout de userState.estimations comme dépendance
+  }, [userState]);
 
-  const handleFilter = (filterValue) => {
-    if (!filterValue.trim()) {
-      setEstimations(initialEstimations);
-      return;
+  useEffect(() => {
+    let sortedAndFilteredData = [...initialEstimations];
+
+    // Filtre
+    if (filterValue.trim()) {
+      sortedAndFilteredData = sortedAndFilteredData.filter(
+        (item) =>
+          item.firstName.toLowerCase().includes(filterValue.toLowerCase()) ||
+          item.lastName.toLowerCase().includes(filterValue.toLowerCase()) ||
+          item.adresse.toLowerCase().includes(filterValue.toLowerCase()) ||
+          item.type.toLowerCase().includes(filterValue.toLowerCase()) ||
+          item.date.toLowerCase().includes(filterValue.toLowerCase()) ||
+          item.email.toLowerCase().includes(filterValue.toLowerCase()) ||
+          item.phone.includes(filterValue)
+      );
     }
 
-    const filteredData = initialEstimations.filter(
-      (item) =>
-        item.firstName.toLowerCase().includes(filterValue.toLowerCase()) ||
-        item.lastName.toLowerCase().includes(filterValue.toLowerCase()) ||
-        item.adresse.toLowerCase().includes(filterValue.toLowerCase()) ||
-        item.type.toLowerCase().includes(filterValue.toLowerCase()) ||
-        item.date.toLowerCase().includes(filterValue.toLowerCase()) ||
-        item.email.toLowerCase().includes(filterValue.toLowerCase()) ||
-        item.phone.includes(filterValue)
-    );
+    // Tri
+    if (sortConfig.column) {
+      sortedAndFilteredData.sort((a, b) => {
+        // Votre logique de tri ici basée sur sortConfig.column
+        // Pour l'instant, je vais supposer une logique simple de tri par chaîne de caractères:
+        if (a[sortConfig.column] > b[sortConfig.column]) return 1;
+        if (a[sortConfig.column] < b[sortConfig.column]) return -1;
+        return 0;
+      });
 
-    setEstimations(filteredData);
+      if (sortConfig.direction === "desc") {
+        sortedAndFilteredData.reverse();
+      }
+    }
+
+    setEstimations(sortedAndFilteredData);
+  }, [filterValue, sortConfig, initialEstimations]);
+
+  const handleFilter = (value) => {
+    setFilterValue(value);
+  };
+
+  const handleSort = (column) => {
+    const newDirection =
+      sortConfig.column === column && sortConfig.direction === "asc"
+        ? "desc"
+        : "asc";
+    setSortConfig({
+      column,
+      direction: newDirection,
+    });
   };
 
   return (
     <DashboardLayout>
-      <div className="w-full flex justify-center mt-5">
+      <div className="w-full flex mt-5">
         <SearchBar onFilter={handleFilter} />
       </div>
-      <Tables data={estimations} />
+      <Tables
+        data={estimations}
+        onSort={handleSort}
+        sortedColumn={{
+          name: sortConfig.column,
+          direction: sortConfig.direction,
+        }}
+      />
     </DashboardLayout>
   );
 };
